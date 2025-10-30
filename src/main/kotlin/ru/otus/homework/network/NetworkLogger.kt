0f718e3d09ca0ddfa58f204d829016b391f43e95
olga <ola.skorp.developer@gmail.com>
2025-10-30 19:03:25 +0100

    Homework 1 and 2

--- src/main/kotlin/ru/otus/homework/network/NetworkLogger.kt
+++ src/main/kotlin/ru/otus/homework/network/NetworkLogger.kt
@@ -15,9 +15,9 @@ sealed class ApiException(message: String) : Throwable(message) {
     data object UnknownException: ApiException("Unknown exception")
 }
 
-class ErrorLogger<E : Throwable> {
+class ErrorLogger<in E : Throwable> {
 
-    val errors = mutableListOf<Pair<LocalDateTime, E>>()
+    private val errors = mutableListOf<Pair<LocalDateTime, E>>()
 
     fun log(response: NetworkResponse<*, E>) {
         if (response is Failure) {
